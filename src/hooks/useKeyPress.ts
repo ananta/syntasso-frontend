1968c8692ba36e2702cf928ff5ad209ffce97fb4
ananta <anantarajbastola@gmail.com>
2020-06-13 00:13:54 +0545

    Add hook to keep track of keyboard pressed sym.

---
+++ src/hooks/useKeyPress.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+
+const useKeyPress = (targetKey: string) => {
+    const [keyPress, setKeyPress] = useState(false);
+    const onKeyDownHandler = ({ key }) => {
+        if (key === targetKey) {
+            setKeyPress(true);
+        }
+    };
+    const onKeyUpHandler = ({ key }) => {
+        if (key === targetKey) {
+            setKeyPress(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('keydown', onKeyDownHandler);
+        window.addEventListener('keyup', onKeyUpHandler);
+        return () => {
+            window.removeEventListener('keydown', onKeyDownHandler);
+            window.removeEventListener('keyup', onKeyUpHandler);
+        };
+    }, []);
+    return keyPress;
+};
+
+export default useKeyPress;
