5ce01319af91175e34953f17c94be8f2515af046
ananta <anantarajbastola@gmail.com>
2020-06-18 23:08:36 +0545

    remove react-alert & add react-toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import { transitions, positions, Provider as AlertProvider } from 'react-alert';
-
 import { PersistGate } from 'redux-persist/integration/react';
 import './styles/app.css';
 import './App.css';
@@ -13,8 +11,8 @@ import 'typeface-roboto';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
 import Routes from './routes';
-import CustomAlertTemplate from 'components/Common/AlertTemplate';
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 // Layouts and Routes
 import PublicLayout from 'shared/layout/PublicLayout';
 import AuthLayout from 'shared/layout/AuthLayout';
@@ -70,47 +68,41 @@ const pages = [
     },
 ];
 
-const AlertOptions = {
-    position: positions.BOTTOM_CENTER,
-    timeout: 4000,
-    offset: '30px',
-    transition: transitions.FADE,
-    containerStyle: {
-        width: '100%',
-        zIndex: 1000,
-        display: 'block',
-        position: 'fixed',
-        right: 0,
-        justifyContent: 'center',
-    },
-};
-
 const App: React.FC = () => {
     const history = createBrowserHistory();
 
     return (
-        <AlertProvider template={CustomAlertTemplate} {...AlertOptions}>
-            <PersistGate loading={null} persistor={persistor}>
-                <Router history={history}>
-                    <Switch>
-                        {pages.map(({ exact, path, component: Component, layout: Layout }, index) => (
-                            <Route
-                                key={index}
-                                exact={exact}
-                                path={path}
-                                render={(props) => (
-                                    <Layout history={props.history} match={props.match} location={props.location}>
-                                        <Component {...props} />
-                                    </Layout>
-                                )}
-                            />
-                        ))}
-                        <Redirect to={Routes.dashboard} />
-                        {/* <Route component={NotFoundPage} /> */}
-                    </Switch>
-                </Router>
-            </PersistGate>
-        </AlertProvider>
+        <PersistGate loading={null} persistor={persistor}>
+            <Router history={history}>
+                <Switch>
+                    {pages.map(({ exact, path, component: Component, layout: Layout }, index) => (
+                        <Route
+                            key={index}
+                            exact={exact}
+                            path={path}
+                            render={(props) => (
+                                <Layout history={props.history} match={props.match} location={props.location}>
+                                    <Component {...props} />
+                                </Layout>
+                            )}
+                        />
+                    ))}
+                    <Redirect to={Routes.dashboard} />
+                    {/* <Route component={NotFoundPage} /> */}
+                </Switch>
+            </Router>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            />
+        </PersistGate>
     );
 };
 
