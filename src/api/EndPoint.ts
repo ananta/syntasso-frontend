0b92e76fa4035344b9c939342caa8f18d6d4c1d8
ananta <anantarajbastola@gmail.com>
2020-07-01 18:46:39 +0545

    Update API endpoints

--- src/api/EndPoint.ts
+++ src/api/EndPoint.ts
@@ -1,10 +1,18 @@
 const API_URL = '127.0.0.1';
-const PORT_NODE = '8080';
+const PORT_NODE = '4000';
 
 const BASE_URL = `http://${API_URL}:${PORT_NODE}`;
 
 const EndPoint = {
     Execute: '/execute',
+    Register: '/register',
+    Login: '/login',
+    Search: '/search',
+    Profile: '/profile',
+    Challenges: '/challenges/',
+    Editorial: '/editorial',
+    Submission: '/submission',
+    Testcase: '/testcase',
 };
 
 export { BASE_URL };
