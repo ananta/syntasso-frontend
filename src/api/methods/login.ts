261393ae2d2c9aecfd575685ce516e3611435b59
ananta <anantarajbastola@gmail.com>
2020-07-01 18:46:05 +0545

    Add Login.

---
+++ src/api/methods/login.ts
@@ -0,0 +1,17 @@
+import makeRequest from 'api/makeRequest';
+import EndPoint from 'api/EndPoint';
+
+interface loginProps {
+    username: string;
+    password: string;
+}
+
+const login = ({ username, password }: loginProps) => {
+    return makeRequest({
+        method: 'post',
+        path: EndPoint.Login,
+        data: { username, password },
+    });
+};
+
+export default login;
