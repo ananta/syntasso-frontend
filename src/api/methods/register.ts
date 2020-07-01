46b39e12534eff7031f17ac70dcc9d107e06f2d4
ananta <anantarajbastola@gmail.com>
2020-07-01 18:46:13 +0545

    Add register.

---
+++ src/api/methods/register.ts
@@ -0,0 +1,20 @@
+import makeRequest from 'api/makeRequest';
+import EndPoint from 'api/EndPoint';
+
+interface registerProps {
+    firstName: string;
+    lastName: string;
+    email: string;
+    username: string;
+    password: string;
+}
+
+const register = (regInfo: registerProps) => {
+    return makeRequest({
+        method: 'post',
+        path: EndPoint.Register,
+        data: { ...regInfo },
+    });
+};
+
+export default register;
