1a13490eae51b23882476a4785d49c4188da586c
ananta <anantarajbastola@gmail.com>
2020-07-04 14:22:09 +0545

    Add UI for Testcase

--- src/pages/Authenticated/Administration/Challenges/Edit/Testcase.tsx
+++ src/pages/Authenticated/Administration/Challenges/Edit/Testcase.tsx
@@ -3,8 +3,14 @@ import { RouteComponentProps, Redirect, useRouteMatch } from 'react-router-dom';
 import { MediumTitle } from 'components/Common/CustomText';
 import Button from 'components/Common/Button';
 import Modal from 'react-modal';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { createTestcase, getChallengeTestcase } from 'api';
+
 import { history } from 'utils/History';
 import InfoWithButton from 'components/Common/InfoWithButton';
+import { Auth } from 'actions/ActionTypes';
+import { toast } from 'react-toastify';
 
 const customStyles = {
     overlay: {
@@ -18,22 +24,97 @@ const customStyles = {
     },
 };
 
-const Testcase: React.FC<RouteComponentProps> = (RouteProps) => {
+interface RouteWithProps extends RouteComponentProps {
+    challengeId: string;
+}
+
+const Testcase: React.FC<RouteWithProps> = (RouteProps) => {
+    const { challengeId } = RouteProps;
+
+    const dispatch = useDispatch();
     const { url } = useRouteMatch();
     let subtitle;
+    const [isAddingTestcase, setIsAddingTestcase] = useState(false);
+    const [testcases, setTestcases] = useState([]);
+
+    const [isGettingTestcase, setIsGettingTestcase] = useState(false);
+
     const [modalIsOpen, setIsOpen] = React.useState(false);
+    const [viewTestCase, setViewTestCase] = React.useState({
+        testcase: {
+            testcaseId: '',
+            sampleInput: '',
+            weightage: '',
+            sampleOutput: '',
+        },
+        isVisible: false,
+    });
     function openModal() {
         setIsOpen(true);
     }
+    const [input, setInput] = useState({
+        weightage: '',
+        sampleInput: '',
+        sampleOutput: '',
+    });
 
-    function afterOpenModal() {
-        // references are now sync'd and can be accessed.
-        // subtitle.style.color = '#f00';
-    }
+    const handleChange = (name, value) => {
+        setInput((input) => ({
+            ...input,
+            [name]: value,
+        }));
+    };
+    const AuthState = useSelector((state) => state['Auth'].data);
 
     function closeModal() {
         setIsOpen(false);
     }
+    const getTestCase = async () => {
+        try {
+            setIsGettingTestcase(true);
+            const testCasesRes = await getChallengeTestcase({
+                token: AuthState.user.token,
+                challengeId,
+            });
+            if (!testCasesRes.isSuccess) throw new Error(testCasesRes.message);
+            setTestcases(testCasesRes.response.testcases);
+            console.log(testCasesRes);
+            setIsGettingTestcase(true);
+        } catch (err) {
+            toast.error(err.message);
+            setIsGettingTestcase(false);
+        }
+    };
+
+    const addTestCase = async () => {
+        try {
+            setIsAddingTestcase(true);
+            const newTestcase = {
+                weightage: Number(input.weightage),
+                sampleInput: input.sampleInput.trim().replace(/\u21b5/g, '\n'),
+                sampleOutput: input.sampleOutput.trim().replace(/\u21b5/g, '\n'),
+            };
+            const addTestCaseRes = await createTestcase({
+                token: AuthState.user.token,
+                challengeId,
+                testcase: newTestcase,
+            });
+            if (!addTestCaseRes.isSuccess) throw new Error(addTestCaseRes.message);
+
+            setIsOpen(false);
+            setIsAddingTestcase(false);
+            toast.success('Testcase Addedd Successfully!');
+            console.log(newTestcase);
+            getTestCase();
+        } catch (err) {
+            setIsOpen(false);
+            toast.error(err.message);
+            setIsAddingTestcase(false);
+        }
+    };
+    useEffect(() => {
+        getTestCase();
+    }, []);
     return (
         <div>
             <InfoWithButton onClick={() => openModal()} title="Create Testcase">
@@ -45,50 +126,55 @@ const Testcase: React.FC<RouteComponentProps> = (RouteProps) => {
             <div>
                 <div className="py-8 w-full">
                     <div className="shadow overflow-hidden rounded border-b border-gray-200">
-                        <table className="min-w-full bg-white">
-                            <thead className="bg-gray-700 text-white">
-                                <tr>
-                                    <th className="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">Name</th>
-                                    <th className="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">
-                                        Points
-                                    </th>
-                                    <th className="text-left py-3 px-4 uppercase font-semibold text-sm">Info</th>
-                                    <th className="text-left py-3 px-4 uppercase font-semibold text-sm">Updates</th>
-                                </tr>
-                            </thead>
-                            <tbody className="text-gray-700">
-                                <tr>
-                                    <td className="w-1/3 text-left py-3 px-4">Test #1</td>
-                                    <td className="w-1/3 text-left py-3 px-4">10</td>
-                                    <td className="text-left py-3 px-4">
-                                        <a className="hover:text-blue-500" href="tel:622322662">
-                                            View
-                                        </a>
-                                    </td>
-                                    <td className="text-left py-3 px-4">
-                                        <a className="hover:text-red-500 text-red-600 ">Remove</a>
-                                    </td>
-                                </tr>
-                                <tr className="bg-gray-100">
-                                    <td className="w-1/3 text-left py-3 px-4">Test #2</td>
-                                    <td className="w-1/3 text-left py-3 px-4">15</td>
-                                    <td className="text-left py-3 px-4">
-                                        <a className="hover:text-blue-500" href="tel:622322662">
-                                            View
-                                        </a>
-                                    </td>
-                                    <td className="text-left py-3 px-4">
-                                        <a className="hover:text-red-500 text-red-600 ">Remove</a>
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </table>
+                        {testcases.length === 0 ? (
+                            <div className="full-width flex justify-center content-center"></div>
+                        ) : (
+                            <table className="min-w-full bg-white">
+                                <thead className="bg-gray-700 text-white">
+                                    <tr>
+                                        <th className="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">
+                                            Name
+                                        </th>
+                                        <th className="w-1/3 text-left py-3 px-4 uppercase font-semibold text-sm">
+                                            Points
+                                        </th>
+                                        <th className="text-left py-3 px-4 uppercase font-semibold text-sm">Info</th>
+                                        <th className="text-left py-3 px-4 uppercase font-semibold text-sm">Updates</th>
+                                    </tr>
+                                </thead>
+                                <tbody className="text-gray-700">
+                                    {testcases.map((testcase, indx) => (
+                                        <tr key={indx.toString()}>
+                                            <td className="w-1/3 text-left py-3 px-4">Test #{testcase.testcaseId}</td>
+                                            <td className="w-1/3 text-left py-3 px-4">{testcase.weightage}</td>
+                                            <td className="text-left py-3 px-4">
+                                                <div
+                                                    className="hover:text-blue-500 cursor-pointer"
+                                                    onClick={() => {
+                                                        setViewTestCase((state) => ({
+                                                            ...state,
+                                                            isVisible: true,
+                                                            testcase,
+                                                        }));
+                                                    }}
+                                                >
+                                                    View
+                                                </div>
+                                            </td>
+                                            <td className="text-left py-3 px-4">
+                                                <a className="hover:text-red-500 text-red-600 ">Remove</a>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        )}
                     </div>
                 </div>
             </div>
             <Modal
                 isOpen={modalIsOpen}
-                onAfterOpen={afterOpenModal}
+                // onAfterOpen={afterOpenModal}
                 onRequestClose={closeModal}
                 style={customStyles}
                 shouldCloseOnOverlayClick={false}
@@ -116,22 +202,6 @@ const Testcase: React.FC<RouteComponentProps> = (RouteProps) => {
                                 className="block tracking-wide text-gray-700 text-sm mb-2 w-full md:w-1/5 font-bold"
                                 htmlFor="grid-text-1"
                             >
-                                Testcase Name
-                            </label>
-                            <input
-                                className="flex flex-1 appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500"
-                                id="grid-text-1"
-                                type="text"
-                                required
-                                onChange={(e) => false}
-                                // value={input.name}
-                            />
-                        </div>
-                        <div className="md:flex w-full md:w-full px-3 mb-6 items-center">
-                            <label
-                                className="block tracking-wide text-gray-700 text-sm mb-2 w-full md:w-1/5 font-bold"
-                                htmlFor="grid-text-1"
-                            >
                                 Testcase Points
                             </label>
                             <input
@@ -139,8 +209,8 @@ const Testcase: React.FC<RouteComponentProps> = (RouteProps) => {
                                 id="grid-text-1"
                                 type="text"
                                 required
-                                onChange={(e) => false}
-                                // value={input.name}
+                                onChange={(e) => handleChange('weightage', e.target.value)}
+                                value={input.weightage}
                             />
                         </div>
                         <div className="md:flex w-full md:w-full px-3 mb-6 ">
@@ -154,8 +224,8 @@ const Testcase: React.FC<RouteComponentProps> = (RouteProps) => {
                                 rows={8}
                                 className="flex flex-1 appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500"
                                 required
-                                onChange={(e) => false}
-                                // value={input.name}
+                                onChange={(e) => handleChange('sampleInput', e.target.value)}
+                                value={input.sampleInput}
                             />
                         </div>
                         <div className="md:flex w-full md:w-full px-3 mb-6 ">
@@ -169,17 +239,105 @@ const Testcase: React.FC<RouteComponentProps> = (RouteProps) => {
                                 rows={8}
                                 className="flex flex-1 appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500"
                                 required
-                                onChange={(e) => false}
-                                // value={input.name}
+                                onChange={(e) => handleChange('sampleOutput', e.target.value)}
+                                value={input.sampleOutput}
                             />
                         </div>
                     </div>
                     <div className="flex justify-between pt-2">
                         <div>
-                            <Button title="Cancel" color="gray-700" onClick={() => closeModal()} />
+                            <Button
+                                title="Cancel"
+                                disabled={isAddingTestcase}
+                                color="gray-700"
+                                onClick={() => closeModal()}
+                            />
                         </div>
                         <div>
-                            <Button title="Confirm" />
+                            <Button title="Confirm" onClick={addTestCase} isBusy={isAddingTestcase} />
+                        </div>
+                    </div>
+                </div>
+            </Modal>
+            <Modal
+                isOpen={viewTestCase.isVisible}
+                onRequestClose={() =>
+                    setViewTestCase((state) => ({
+                        ...state,
+                        isVisible: false,
+                    }))
+                }
+                style={customStyles}
+                shouldCloseOnOverlayClick={false}
+                contentLabel="Example Modal"
+            >
+                <div className="modal-content py-4 text-left px-6 w-full">
+                    <div className="flex justify-between items-center pb-3 w-full">
+                        <p className="text-2xl font-bold">View Test Case</p>
+                        <div className="modal-close cursor-pointer z-50">
+                            <svg
+                                onClick={() => {
+                                    setViewTestCase((state) => ({
+                                        ...state,
+                                        isVisible: false,
+                                    }));
+                                }}
+                                className="fill-current text-black"
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="18"
+                                height="18"
+                                viewBox="0 0 18 18"
+                            >
+                                <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+                            </svg>
+                        </div>
+                    </div>
+                    <div className="my-5 ">
+                        <div className="md:flex w-full md:w-full px-3 mb-6 items-center">
+                            <label
+                                className="block tracking-wide text-gray-700 text-sm mb-2 w-full md:w-1/5 font-bold"
+                                htmlFor="grid-text-1"
+                            >
+                                Testcase Points
+                            </label>
+                            <input
+                                className="flex flex-1 appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500"
+                                id="grid-text-1"
+                                type="text"
+                                required
+                                // onChange={(e) => handleChange('weightage', e.target.value)}
+                                value={viewTestCase.testcase.weightage}
+                            />
+                        </div>
+                        <div className="md:flex w-full md:w-full px-3 mb-6 ">
+                            <label
+                                className="block tracking-wide text-gray-700 text-sm mb-2 w-full md:w-1/5 font-bold"
+                                htmlFor="grid-text-1"
+                            >
+                                Input:
+                            </label>
+                            <textarea
+                                rows={8}
+                                className="flex flex-1 appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500"
+                                required
+                                onChange={(e) => handleChange('sampleInput', e.target.value)}
+                                value={viewTestCase.testcase.sampleInput}
+                            />
+                        </div>
+                        <div className="md:flex w-full md:w-full px-3 mb-6 ">
+                            <label
+                                className="block tracking-wide text-gray-700 text-sm mb-2 w-full md:w-1/5 font-bold"
+                                htmlFor="grid-text-1"
+                            >
+                                Output:
+                            </label>
+                            <textarea
+                                rows={8}
+                                className="flex flex-1 appearance-none block w-full bg-white text-gray-700 border border-gray-400 shadow-inner rounded-md py-3 px-4 leading-tight focus:outline-none  focus:border-gray-500"
+                                required
+                                onChange={(e) => handleChange('sampleOutput', e.target.value)}
+                                value={viewTestCase.testcase.sampleOutput}
+                            />
                         </div>
                     </div>
                 </div>
