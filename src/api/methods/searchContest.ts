0359937de886a8d5d2dc4eff2a21d1bb9faf45a6
ananta <anantarajbastola@gmail.com>
2020-12-15 11:17:38 +0545

    Update filter route in search api

--- src/api/methods/searchContest.ts
+++ src/api/methods/searchContest.ts
@@ -6,15 +6,18 @@ interface searchContestProps {
   page: number;
   limit: number;
   query?: string;
+  status?: string;
   type: string;
 }
 
-const searchContest = ({ token, limit, page, query, type }: searchContestProps) => {
+const searchContest = ({ token, limit, page, query, type, status }: searchContestProps) => {
   return makeRequest({
     method: 'get',
     path:
       EndPoint.Search +
-      `?type=${type}&${query && query.length > 1 ? 'query=' + query + '&' : ''}page=${page}&limit=${limit}`,
+      `?type=${type}&${
+        query && query.length > 1 ? 'query=' + query + '&' : ''
+      }page=${page}&limit=${limit}&status=${status}`,
     token,
     data: {},
   });
