263acd1df9029b281c380cef482bd58bf741d115
ananta <anantarajbastola@gmail.com>
2021-07-06 14:28:04 +0545

    feat(utilities): add uitility functions for color generation from a set of string

---
+++ src/utils/Bits.ts
@@ -0,0 +1,29 @@
+function hashCode(str) {
+  // java String#hashCode
+  var hash = 0;
+  for (var i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  return hash;
+}
+
+function intToRGB(i) {
+  var c = (i & 0x00ffffff).toString(16).toUpperCase();
+
+  return '00000'.substring(0, 6 - c.length) + c;
+}
+
+export { hashCode, intToRGB };
+
+export const stringToColour = function (str) {
+  var hash = 0;
+  for (var i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  var colour = '#';
+  for (var i = 0; i < 3; i++) {
+    var value = (hash >> (i * 8)) & 0xff;
+    colour += ('00' + value.toString(16)).substr(-2);
+  }
+  return colour;
+};
