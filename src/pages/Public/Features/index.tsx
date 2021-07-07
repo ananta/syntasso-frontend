e2d0d54268f59d4288b0c12a518b4d49c4516d65
Bibek <bibekbhujel75@gmail.com>
2021-07-07 20:54:31 +0545

    minor changes

--- src/pages/Public/Features/index.tsx
+++ src/pages/Public/Features/index.tsx
@@ -7,11 +7,15 @@ const Features = () => {
         <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold leading-tight text-gray-900">Features</h1>
           <br />
-           <ul className="list-disc mx-auto">
-             <li>Cheap</li>
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam eveniet fuga in voluptates illum, id sapiente libero culpa officiis nesciunt fugit voluptas quod architecto commodi eos. Eligendi necessitatibus optio impedit!</p>
+           <p>We take pride in syntasso being:</p>
+           <br />
+           <ul className="list-disc list-inside">
              <li>Fast</li>
+             <li>Cheap</li>
              <li>Reliable</li>
            </ul>
+           
         </div>
       </header>
      </div>
