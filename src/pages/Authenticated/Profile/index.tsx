6a3427f63a7c2b13d044135ca376651a0f2b057f
ananta <anantarajbastola@gmail.com>
2021-07-07 22:02:28 +0545

    feat(pdf): add endpoints for pdf generation

--- src/pages/Authenticated/Profile/index.tsx
+++ src/pages/Authenticated/Profile/index.tsx
@@ -17,6 +17,7 @@ import { MdAccountCircle } from 'react-icons/md';
 import TimelineListItem from './Components/TimelineListItem';
 import getUserTimeline from 'api/methods/getUserTimeline';
 import usePaginatedFetcher from 'hooks/usePaginatedFetcher';
+import { BASE_URL } from 'api/EndPoint';
 
 type IUserInfo = {
   userId: string;
@@ -390,12 +391,14 @@ const Profile: React.FC<RouteComponentProps> = (RouteProps) => {
                     </ul>
                   </div>
                   <div className="mt-6 flex flex-col justify-stretch">
-                    <button
+                    <a
+                      href={`${BASE_URL}/timeline/${userInfo.username}`}
+                      target="__blank"
                       type="button"
                       className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                     >
                       Generate Portfolio
-                    </button>
+                    </a>
                   </div>
                 </div>
               </section>
