817ab30ec4a9953cc5be919f41ec23200c0357b4
Bibek <bibekbhujel75@gmail.com>
2021-07-07 10:49:32 +0545

    position fixes

--- src/pages/Public/Features/index.tsx
+++ src/pages/Public/Features/index.tsx
@@ -3,8 +3,18 @@ import React from 'react';
 const Features = () => {
   return (
     <div>
-      <h1>Features</h1>
-    </div>
+      <header className="bg-white shadow">
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold leading-tight text-gray-900">Features</h1>
+          <br />
+           <ul className="list-disc mx-auto">
+             <li>Cheap</li>
+             <li>Fast</li>
+             <li>Reliable</li>
+           </ul>
+        </div>
+      </header>
+     </div>
   );
 };
 
