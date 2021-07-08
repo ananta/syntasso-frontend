889b8413b963d8f10c0132ee7b00d76ad5e765f7
Bibek <bibekbhujel75@gmail.com>
2021-07-08 10:47:35 +0545

    added svg and layout changes

--- src/pages/Public/Features/index.tsx
+++ src/pages/Public/Features/index.tsx
@@ -4,30 +4,59 @@ const Features = () => {
   return (
     <div>
       <header className="bg-white shadow">
-        {/* <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold leading-tight text-gray-900">Features</h1>
-          <br />
-           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam eveniet fuga in voluptates illum, id sapiente libero culpa officiis nesciunt fugit voluptas quod architecto commodi eos. Eligendi necessitatibus optio impedit!</p>
-           <p>We take pride in syntasso being:</p>
-           <br />
-           <ul className="list-disc list-inside">
-             <li>Fast</li>
-             <li>Cheap</li>
-             <li>Reliable</li>
-           </ul>
-           
-        </div> */}
-
-        <section className="text-gray-600 body-font">
+          <section className="text-gray-600 body-font">
   <div className="container px-5 py-20 mx-auto">
     <div className="flex flex-col text-center w-full mb-20">      
       <h1 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">Our Features</h1>
     </div>
     <div className="flex flex-wrap -m-4">
       <div className="p-4 md:w-1/3">
-        <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
-          <div className="flex items-center mb-3 text-center">           
-            <h2 className="text-gray-900 text-lg title-font font-medium ">Fast</h2>
+        <div className="flex rounded-lg h-full bg-gray-200 p-8 flex-col">
+          <div className="flex flex-row content-center justify-center">
+        <div className="w-16 h-16 pr-1">
+        <svg
+          className="fill-current "
+          width="100%"
+          height="100%"
+          viewBox="0 0 48 48"
+          version="1.1"
+          style={{
+            fillRule: 'evenodd',
+            clipRule: 'evenodd',
+            strokeLinejoin: 'round',
+            strokeMiterlimit: 2,
+          }}
+        >
+          <path
+            d="M13.804,30.538l-7.109,5.752c-0.215,0.174 -0.248,0.49 -0.074,0.703c0.099,0.123 0.243,0.188 0.389,0.188c0.11,0 0.222,-0.037 0.314,-0.111l7.109,-5.754c0.215,-0.174 0.248,-0.487 0.074,-0.703c-0.172,-0.216 -0.485,-0.249 -0.703,-0.075Z"
+            style={{ fillRule: 'nonzero' }}
+          ></path>
+          <path
+            d="M45.977,5.392c-0.012,-0.031 -0.023,-0.059 -0.041,-0.086c-0.016,-0.028 -0.027,-0.057 -0.051,-0.081c-0.02,-0.024 -0.043,-0.044 -0.066,-0.064c-0.023,-0.021 -0.047,-0.041 -0.076,-0.058c-0.027,-0.016 -0.063,-0.024 -0.094,-0.036c-0.021,-0.007 -0.041,-0.021 -0.064,-0.022c-0.01,-0.003 -0.02,0.002 -0.029,0.001c-0.008,-0.001 -0.018,-0.008 -0.025,-0.008c-3.681,-0.176 -7.298,0.262 -10.748,1.204c-0.029,-0.001 -0.059,-0.01 -0.09,-0.005c-0.078,0.013 -0.146,0.047 -0.207,0.09c-9.127,2.582 -17.019,8.825 -21.484,17.529c-0.001,0 -0.001,0.001 -0.001,0.001c-0.001,0.002 -0.003,0.005 -0.004,0.008c-0.012,0.022 -0.013,0.048 -0.021,0.071c-0.012,0.037 -0.027,0.072 -0.031,0.11c-0.003,0.034 0.005,0.065 0.01,0.102c0.003,0.03 0.001,0.061 0.01,0.089c0.014,0.044 0.038,0.08 0.063,0.118c0.01,0.018 0.014,0.036 0.026,0.052l0.006,0.007l7.216,8.919c0.018,0.021 0.044,0.031 0.064,0.051c0.02,0.02 0.036,0.031 0.059,0.047c0.081,0.053 0.171,0.088 0.266,0.088c0.043,0 0.087,-0.004 0.13,-0.018c9.48,-2.559 17.259,-9.001 21.683,-17.435c0.027,-0.037 0.066,-0.062 0.086,-0.104c0.01,-0.025 0.008,-0.052 0.012,-0.077c1.645,-3.18 2.826,-6.627 3.426,-10.269c0.002,-0.011 -0.002,-0.02 -0.002,-0.028c0.002,-0.01 0.008,-0.02 0.008,-0.028c0,-0.023 -0.008,-0.045 -0.012,-0.068c-0.012,-0.036 -0.013,-0.069 -0.019,-0.1Zm-1.084,0.635c-0.584,3.206 -1.619,6.261 -3.047,9.092c-3.338,-1.445 -5.766,-4.445 -6.479,-8.012c3.067,-0.802 6.268,-1.176 9.526,-1.08Zm-24.056,26.413l-6.796,-8.4c4.305,-8.199 11.741,-14.121 20.362,-16.657c0.797,3.826 3.396,7.042 6.975,8.62c-4.282,7.9 -11.624,13.939 -20.541,16.437Z"
+            style={{ fillRule: 'nonzero' }}
+          ></path>
+          <path style={{ fillRule: 'nonzero' }}></path>
+          <path
+            d="M2.528,20.427c3.118,-0.176 6.283,0.477 9.077,1.865c0.071,0.035 0.147,0.052 0.223,0.052c0.176,0 0.346,-0.093 0.437,-0.256c1.617,-2.901 3.624,-5.564 5.965,-7.916c0.139,-0.14 0.183,-0.347 0.113,-0.53c-0.07,-0.185 -0.241,-0.311 -0.437,-0.322c-5.925,-0.371 -11.74,1.959 -15.771,6.268c-0.14,0.147 -0.174,0.367 -0.088,0.554c0.087,0.182 0.251,0.296 0.481,0.285Zm14.142,-6.145l0.064,0c-1.963,2.097 -3.678,4.413 -5.11,6.907c-2.461,-1.146 -5.174,-1.763 -7.889,-1.791c3.489,-3.264 8.136,-5.116 12.935,-5.116Z"
+            style={{ fillRule: 'nonzero' }}
+          ></path>
+          <path
+            d="M22.149,44.983c0.089,0.065 0.195,0.102 0.302,0.102c0.089,0 0.179,-0.023 0.258,-0.072c5.106,-3.074 8.54,-8.219 9.42,-14.115c0.025,-0.194 -0.061,-0.389 -0.227,-0.494c-0.164,-0.104 -0.375,-0.106 -0.541,0c-2.791,1.8 -5.813,3.205 -8.985,4.181c-0.256,0.08 -0.404,0.347 -0.338,0.604c0.777,3.037 0.753,6.242 -0.071,9.268c-0.052,0.194 0.017,0.403 0.182,0.526Zm8.793,-13.14c-0.99,4.74 -3.742,8.891 -7.718,11.646c0.539,-2.668 0.506,-5.434 -0.104,-8.09c2.738,-0.878 5.363,-2.072 7.822,-3.556Z"
+            style={{ fillRule: 'nonzero' }}
+          ></path>
+          <path
+            d="M12.12,28.351l-7.87,4.818c-0.235,0.145 -0.31,0.451 -0.166,0.688c0.095,0.154 0.259,0.24 0.427,0.24c0.09,0 0.18,-0.025 0.261,-0.074l7.87,-4.817c0.235,-0.146 0.31,-0.451 0.166,-0.688c-0.145,-0.238 -0.454,-0.31 -0.688,-0.167Z"
+            style={{ fillRule: 'nonzero' }}
+          ></path>
+          <path
+            d="M9.161,40.13c0.097,0.092 0.221,0.139 0.345,0.139c0.132,0 0.265,-0.053 0.362,-0.156l6.354,-6.688c0.19,-0.198 0.183,-0.518 -0.018,-0.707c-0.2,-0.188 -0.519,-0.184 -0.707,0.021l-6.354,6.688c-0.19,0.197 -0.182,0.511 0.018,0.703Z"
+            style={{ fillRule: 'nonzero' }}
+          ></path>
+        </svg>
+      </div>
+          <div className="flex items-center text-center">           
+            <h2 className="text-gray-900 text-2xl title-font font-medium">Fast</h2>
+          </div>
           </div>
           <div className="flex-grow">
             <p className="leading-relaxed text-base">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem incidunt ad perspiciatis. Possimus, inventore. Blanditiis non praesentium ducimus fugiat voluptatibus adipisci architecto! Vel sed quod impedit at? Nemo, culpa harum.</p>
@@ -40,10 +69,41 @@ const Features = () => {
         </div>
       </div>
       <div className="p-4 md:w-1/3">
-        <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
-          <div className="flex items-center mb-3">
-           
-            <h2 className="text-gray-900 text-lg title-font font-medium">Reliable</h2>
+        <div className="flex rounded-lg h-full bg-gray-200 p-8 flex-col">
+          <div className="flex flex-row content-center justify-center">
+        <div className="w-16 h-16 p-2">
+        <svg
+          className="fill-current "
+          width="100%"
+          height="100%"
+          viewBox="0 0 30 30"
+          version="1.1"
+          style={{
+            fillRule: 'evenodd',
+            clipRule: 'evenodd',
+            strokeLinejoin: 'round',
+            strokeMiterlimit: 2,
+          }}
+        >
+          <g>
+            <path
+              d="M8,11l0,2l4,0l0,2l-4,0l0,6l6,0l0,-2l-4,0l0,-2l4,0l0,-6l-6,0Z"
+              style={{ fillRule: 'nonzero' }}
+            ></path>
+            <path
+              d="M21,15l0,-4l-2,0l0,4l-1,0l0,-4l-2,0l0,6l3,0l0,4l2,0l0,-4l2,0l0,-2l-2,0Z"
+              style={{ fillRule: 'nonzero' }}
+            ></path>
+            <path
+              d="M26.917,10.799l-1.832,0.801c0.607,1.389 0.915,2.869 0.915,4.4c0,6.065 -4.935,11 -11,11c-6.065,0 -11,-4.935 -11,-11c0,-6.065 4.935,-11 11,-11l0,2l5,-3l-5,-3l0,2c-7.168,0 -13,5.832 -13,13c0,7.168 5.832,13 13,13c7.169,0 13,-5.832 13,-13c0,-1.81 -0.364,-3.56 -1.083,-5.201Z"
+              style={{ fillRule: 'nonzero' }}
+            ></path>
+          </g>
+        </svg>
+      </div>
+          <div className="flex items-center">           
+            <h2 className="text-gray-900 text-2xl title-font font-medium">Reliable</h2>
+          </div>
           </div>
           <div className="flex-grow">
             <p className="leading-relaxed text-base">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam dignissimos vitae est nisi ratione fugit autem tempore consequatur, iure nemo doloremque dolorum obcaecati cupiditate sed tempora nihil aspernatur excepturi? Nobis.</p>
@@ -56,10 +116,31 @@ const Features = () => {
         </div>
       </div>
       <div className="p-4 md:w-1/3">
-        <div className="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
-          <div className="flex items-center mb-3">
-           
-            <h2 className="text-gray-900 text-lg title-font font-medium">Low Price</h2>
+        <div className="flex rounded-lg h-full bg-gray-200 p-8 flex-col">
+          <div className="flex justify-center align-center">
+        <div className="w-16 h-16 ">
+        <svg
+          className="fill-current "
+          width="100%"
+          height="100%"
+          viewBox="0 0 64 65"
+          version="1.1"
+          style={{
+            fillRule: 'evenodd',
+            clipRule: 'evenodd',
+            strokeLinejoin: 'round',
+            strokeMiterlimit: 2,
+          }}
+        >
+          <path
+            d="M29,33.997c0.002,1.645 1.355,2.998 3,3c2.743,0 5,2.257 5,5c0,2.367 -1.681,4.425 -4,4.899l0,1.101c0,0.549 -0.451,1 -1,1c-0.549,0 -1,-0.451 -1,-1l0,-1.101c-2.317,-0.476 -3.997,-2.533 -4,-4.899c0,-0.549 0.451,-1 1,-1c0.549,0 1,0.451 1,1c0,1.646 1.354,3 3,3c1.646,0 3,-1.354 3,-3c0,-1.646 -1.354,-3 -3,-3c-2.743,0 -5,-2.257 -5,-5c0,-2.367 1.681,-4.425 4,-4.899l0,-1.101c0,-0.549 0.451,-1 1,-1c0.549,0 1,0.451 1,1l0,1.101c2.318,0.476 3.997,2.533 4,4.899c0,0.549 -0.451,1 -1,1c-0.549,0 -1,-0.451 -1,-1c0,-1.646 -1.354,-3 -3,-3c-1.646,0 -3,1.354 -3,3Zm25,21.172c-0.003,2.099 -1.729,3.825 -3.828,3.828l-36.344,0c-2.1,0 -3.828,-1.728 -3.828,-3.828c0,-1.015 0.404,-1.99 1.121,-2.707l0.586,-0.586c0.187,-0.188 0.292,-0.442 0.293,-0.707c-0.079,-0.911 -1.809,-23.976 12.648,-35.399l-5.48,-8.218c-0.11,-0.165 -0.168,-0.358 -0.168,-0.555c0,-0.351 0.184,-0.677 0.485,-0.857c7.809,-4.187 17.22,-4.187 25.029,0c0.301,0.18 0.486,0.506 0.486,0.857c0,0.197 -0.059,0.39 -0.168,0.555l-5.48,8.218c14.457,11.423 12.727,34.489 12.645,35.481c0.021,0.237 0.126,0.459 0.296,0.625l0.586,0.586c0.72,0.716 1.124,1.691 1.121,2.707Zm-27.465,-40.172l2.166,0c-0.404,-1.56 -0.938,-3.083 -1.596,-4.554c-0.07,-0.138 -0.106,-0.291 -0.106,-0.447c0,-0.548 0.452,-1 1.001,-1c0.378,0 0.725,0.215 0.895,0.554c0.787,1.755 1.415,3.578 1.874,5.447l2.309,0c0.248,-1.756 1.02,-3.397 2.215,-4.707c0.187,-0.182 0.437,-0.283 0.697,-0.283c0.549,0 1,0.451 1,1c0,0.259 -0.101,0.509 -0.281,0.695c-0.825,0.933 -1.383,2.071 -1.615,3.295l2.371,0l5.084,-7.626c-6.673,-3.163 -14.426,-3.163 -21.099,-0.002l5.085,7.628Zm25.465,40.172c-0.001,-0.485 -0.194,-0.949 -0.535,-1.293l-0.586,-0.586c-0.564,-0.561 -0.881,-1.325 -0.879,-2.121c0.022,-0.317 1.747,-23.548 -12.346,-34.172l-11.308,0c-14.093,10.624 -12.368,33.855 -12.349,34.09c0.021,0.823 -0.296,1.619 -0.876,2.203l-0.586,0.586c-0.342,0.343 -0.535,0.808 -0.535,1.292c0,1.003 0.825,1.829 1.828,1.829l36.344,0c1.002,-0.001 1.827,-0.826 1.828,-1.828Z"
+            style={{ fillRule: 'nonzero' }}
+          ></path>
+        </svg>
+      </div>
+          <div className="flex items-center ">           
+            <h2 className="text-gray-900 text-2xl title-font font-medium align-center justify-center">Low Price</h2>
+          </div>
           </div>
           <div className="flex-grow">
             <p className="leading-relaxed text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi veniam expedita optio minima ut quisquam ipsam. Voluptate architecto molestias rerum aperiam fugiat eum, doloremque id, placeat sed voluptatum et distinctio!</p>
