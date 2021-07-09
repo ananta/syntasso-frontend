756efee4b742024d71cf43620216c201b782ebec
Bibek <bibekbhujel75@gmail.com>
2021-07-09 16:56:53 +0545

    .

--- src/pages/Public/About/index.tsx
+++ src/pages/Public/About/index.tsx
@@ -7,7 +7,7 @@ const About = () => {
         <div className="max-w-7xl mx-auto py-20 px-4 sm:px-6 lg:px-8">
           <h1 className="text-5xl text-left font-medium leading-tight text-gray-900  ">About us</h1>
           <br/>
-          <p className="text-left"> Syntasso is a web-based competitive coding tool. Syntasso aims to help newbies learn code and polish their skills down the line as well. Syntasso provides an opportunity for a measure of skill between peers at a similar level. Syntasso currently supports 3 languages namely C, C++ and JavaScript.Syntasso started out as the Major Project for 3 students at Gandaki College of Engineering and science. The idea came about when the students were discussing about the lack of in house resources for any kind of competitive contests in the college. From there on out, exploration of possibilities and ideas brought us here.</p>
+          <p className="text-left"> Syntasso is a web-based competitive coding tool. Syntasso aims to help newbies learn code and polish their skills down the line as well. Syntasso provides an opportunity for a measure of skill between peers at a similar level. Syntasso currently supports 3 languages namely C, C++ and JavaScript.Syntasso started out as the Major Project for 3 students at Gandaki College of Engineering and Science. The idea came about when the students were discussing about the lack of in house resources for any kind of competitive contests in the college. From there on out, exploration of possibilities and ideas brought us here.</p>
         </div>
       </header>
      
