98481e7deb4e446169db1eb7c945028b0a3f6476
Bibek <bibekbhujel75@gmail.com>
2021-07-09 19:41:28 +0545

    grid fixes

--- src/pages/Authenticated/Dashboard/Components/TagsGrid.tsx
+++ src/pages/Authenticated/Dashboard/Components/TagsGrid.tsx
@@ -1,32 +1,35 @@
 import React from 'react';
-import { GiAtomicSlashes } from 'react-icons/gi';
+import { GiAtomicSlashes,GiProcessor,GiFamilyTree } from 'react-icons/gi';
+import {GrGraphQl} from 'react-icons/gr';
+import {RiLinksLine} from 'react-icons/ri';
 
 const TagsGrid = () => {
   return (
     <>
       <div>
         <div className="">
-          <div className="rounded-lg bg-gray-200 overflow-hidden shadow divide-y divide-gray-200 sm:divide-y-0 sm:grid sm:grid-cols-2 sm:gap-px">
-            <div className="rounded-tl-lg rounded-tr-lg sm:rounded-tr-none    relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
+          <div className="rounded-lg bg-gray-200 overflow-hidden shadow divide-y divide-gray-200 sm:divide-y-0 sm:grid sm:grid-cols-2 sm:gap-px ">
+            <div className="rounded-tl-lg rounded-tr-lg sm:rounded-tr-none relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500 flex ">
               <div>
                 <span className="rounded-lg inline-flex px-3 bg-teal-50 text-teal-700 ring-4 ring-white">
-                  <GiAtomicSlashes className="h-12 w-12" />
+                  <GiProcessor className="h-12 w-12 align-center" />
                 </span>
               </div>
               <div className="">
+                
                 <h3 className="text-lg font-medium">
-                  <a href="/challenges?tag=Algorithms" className="focus:outline-none">
+                  <a href="/challenges?tag=Algorithms" className="focus:outline-none ">
                     <span className="absolute inset-0" aria-hidden="true"></span>
-                    Algorithms
+                    <p className="pt-2">Algorithms</p>
                   </a>
                 </h3>
               </div>
             </div>
 
-            <div className=" sm:rounded-tr-lg   relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
+            <div className=" sm:rounded-tr-lg relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500 flex">
               <div>
                 <span className="rounded-lg inline-flex px-3 bg-teal-50 text-teal-700 ring-4 ring-white">
-                  <GiAtomicSlashes className="h-12 w-12" />
+                  <GrGraphQl className="h-12 w-12" />
                 </span>
               </div>
               <div>
@@ -34,15 +37,15 @@ const TagsGrid = () => {
                   <a href="/challenges?tag=Graphs" className="focus:outline-none">
                     {/* Extend touch target to entire panel */}
                     <span className="absolute inset-0" aria-hidden="true"></span>
-                    Graphs
+                    <p className="pt-2">Graphs</p>
                   </a>
                 </h3>
               </div>
             </div>
-            <div className="  sm:rounded-bl-lg  relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
+            <div className="  sm:rounded-bl-lg  relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500 flex">
               <div>
                 <span className="rounded-lg inline-flex px-3 bg-teal-50 text-teal-700 ring-4 ring-white">
-                  <GiAtomicSlashes className="h-12 w-12" />
+                  <RiLinksLine className="h-12 w-12" />
                 </span>
               </div>
               <div className="">
@@ -50,16 +53,16 @@ const TagsGrid = () => {
                   <a href="/challenges?tag=Linked%20List" className="focus:outline-none">
                     {/* Extend touch target to entire panel */}
                     <span className="absolute inset-0" aria-hidden="true"></span>
-                    Linked List
+                    <p className="pt-2">Linked List</p>
                   </a>
                 </h3>
               </div>
             </div>
 
-            <div className="   rounded-bl-lg rounded-br-lg sm:rounded-bl-none relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
+            <div className="   rounded-bl-lg rounded-br-lg sm:rounded-bl-none relative group bg-white p-6 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500 flex">
               <div>
                 <span className="rounded-lg inline-flex px-3 bg-teal-50 text-teal-700 ring-4 ring-white">
-                  <GiAtomicSlashes className="h-12 w-12" />
+                  <GiFamilyTree className="h-12 w-12" />
                 </span>
               </div>
               <div className="">
@@ -67,7 +70,7 @@ const TagsGrid = () => {
                   <a href="/challenges?tag=Data%20Structures" className="focus:outline-none">
                     {/* Extend touch target to entire panel */}
                     <span className="absolute inset-0" aria-hidden="true"></span>
-                    Data Structures
+                    <p className="pt-2">Data Structures</p>
                   </a>
                 </h3>
               </div>
